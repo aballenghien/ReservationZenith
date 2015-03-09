76e2073cfefb7279c1edc0b1fe6789ac7670e182
aballenghien <audrey.ballenghien@gmail.com>
2015-03-09 23:53:58 +0100

    correction commit

--- src/AB/ReservationZenithBundle/Controller/AjaxController.php
+++ src/AB/ReservationZenithBundle/Controller/AjaxController.php
@@ -6,7 +6,7 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
-class DefaultController extends Controller
+class AjaxController extends Controller
 {
 
 	public function getTarifWithPlace(){
