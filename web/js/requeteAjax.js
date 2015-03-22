6d34c6e34153aea713993b9b26983e8f054ece56
aballenghien <audrey.ballenghien@gmail.com>
2015-03-22 19:03:02 +0100

    modif

--- web/js/requeteAjax.js
+++ web/js/requeteAjax.js
@@ -1,10 +1,10 @@
 /*selection du tarif en fonction de la place*/
 var champPlace = $('#reservation_place');
-var champPrix = $('#reservation_tarif');
 var champSpectacle = $('#reservation_spectacles');
 var champHref = $('#href');
 var champHrefSeance = $('#hrefSeances');
 var champSeance = $('#reservation_seance');
+var champPrix = $('#reservation_tarif');
 $(document).ready(function(){
 champSeance.html(null);
   champPlace.change(function(){
@@ -18,12 +18,11 @@ champSeance.html(null);
        type : 'GET',
        success: function(retour){
         var tarif = JSON.parse(retour);
-        champPrix.val(tarif.id);
+        champPrix.val(tarif.prix);
        }  
     });
   });
-
-  champSpectacle.change(function(){    
+  champSpectacle.click(function(){
     champSeance.html(null);
     var plc = champPlace.val();
     var id = champSpectacle.val();
@@ -36,11 +35,7 @@ champSeance.html(null);
        type : 'GET',
        success: function(retour){
         var tarif = JSON.parse(retour);
-        champPrix.val(tarif.id);
-       },
-       error: function(XMLHttpRequest, textStatus, errorThrown) {
-          alert("Aucun tarif associé, mais le spectacle n'est pas gratuit!"); 
-          champPrix.val("");
+        champPrix.val(tarif.prix)
        }  
     });    
     var unurl = champHrefSeance.val();
@@ -56,11 +51,39 @@ champSeance.html(null);
           var heure = heure.substr(0,5);
           champSeance.append("<option value='"+seances[s].id+"'>"+date+"   "+heure+"</option>");
         }
-       },
-       error: function(XMLHttpRequest, textStatus, errorThrown) { 
-          alert("Aucune séance n'est programmée");
-          champSeance.val("");
        }  
+    });
+  });
+  champSpectacle.change(function(){    
+    champSeance.html(null);
+    var plc = champPlace.val();
+    var id = champSpectacle.val();
+    var unurl = "";
+    unurl = champHref.val();
+    unurl = unurl.replace("__place__",plc);
+    unurl = unurl.replace("__spectacle__",id);
+    $.ajax({
+       url : unurl,
+       type : 'GET',
+       success: function(retour){
+        var tarif = JSON.parse(retour);
+        champPrix.val(tarif.prix)
+       } 
+    });    
+    var unurl = champHrefSeance.val();
+    unurl = unurl.replace("__spectacle__",id);
+    $.ajax({
+       url : unurl,
+       type : 'GET',
+       success: function(retour){
+        var seances = JSON.parse(retour);
+        for(s in seances){
+          var date = seances[s].date.substr(0,10);
+          var heure = seances[s].heure.substr(11,12);
+          var heure = heure.substr(0,5);
+          champSeance.append("<option value='"+seances[s].id+"' selected='true'>"+date+"   "+heure+"</option>");
+        }
+       }
     });
   });
 });
